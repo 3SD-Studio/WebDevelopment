25422d255f10acb48b81873a42c98baf61f92c52
Michał Pilch <m.pilch2002@gmail.com>
2023-04-17 15:21:09 +0200

    Ace editor included

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let htmlEditor;
+let cssEditor;
+let jsEditor;
+
+window.onload = function () {
+    htmlEditor = ace.edit("htmlEditor");
+    htmlEditor.setTheme("ace/theme/monokai");
+    htmlEditor.session.setMode("ace/mode/html");
+
+    cssEditor = ace.edit("cssEditor");
+    cssEditor.setTheme("ace/theme/monokai");
+    cssEditor.session.setMode("ace/mode/css");
+
+    jsEditor = ace.edit("jsEditor");
+    jsEditor.setTheme("ace/theme/monokai");
+    jsEditor.session.setMode("ace/mode/javascript");
+}
+   
